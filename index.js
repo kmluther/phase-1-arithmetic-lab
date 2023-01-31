3db692ec0fc53f669265145d1bc5b15cdd9633bf
Katelyn Luther <katelynluther87@aim.com>
2023-01-30 17:46:28 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+let multiply = num1 * num2;
+
+let random = Math.round(Math.random() *10);
+
+let num3 = 104;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 7, 20);
